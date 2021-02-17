db4b09bd36b1340e88c5b174f75d2dfe34e98c36
StefWebDesigner <67237726+StefWebDesigner@users.noreply.github.com>
2021-02-17 19:48:43 +0000

    Optimised the grid layout > DRY > Finished project

--- javascript/app.js
+++ javascript/app.js
@@ -17,18 +17,141 @@ window.addEventListener('load', function() {
 
 const search = new Filter('search', 'data-caption');
 
-
+//==================================================================================
 
 //Search Filter >>>> It doesn't work But I want to exceed!!!!
+//              >>>> Unable to complete...Family problems and need to move on to the next project
 
-// const search = new Filter('search', 'data-caption');
+//Make a directory
+//????how to link these with getID's >> Add the property link & documeent return link >> Array add amd
 
-// function searchFilter ('search,' 'data-captions') {
+// const photoGallerySet = [
 
-//   let search = document.querySelector('header').innerHTML = search;
-//   let datacap = document.querySelector('header').innerHTML = data-caption;
+// { image: '../photos/01.jpg',
+//     dataCaptions: 'I love hay bales. Took this snap on a drive through the countryside past some straw fields.',
+//     id: pic-1;
+// }, 
+// { image: '../photos/02.jpg',
+//     dataCaptions: 'The lake was so calm today. We had a great view of the snow on the mountains from here',
+//     id: pic-2;
+//   },
+// { image: '../photos/03.jpg',
+//     dataCaptions: 'I hiked to the top of the mountain and got this picture of the canyon and trees below',
+//     id: pic-3;
+//   },
+// { image: '../photos/04.jpg',
+//     dataCaptions: 'It was amazing to see an iceberg up close, it was so cold but didn’t snow today',
+//     id: pic-4;
+//   },
+// { image: '../photos/05.jpg',
+//     dataCaptions: 'The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons',
+//     id: pic-5;
+//   },
+// { image: '../photos/06.jpg',
+//     dataCaptions: 'Fall is coming, I love when the leaves on the trees start to change color',
+//     id: pic-6;
+//   },
+// { image: '../photos/07.jpg',
+//     dataCaptions: 'I drove past this plantation yesterday, everything is so green',
+//     id: pic-7;
+//   },
+// { image: '../photos/08.jpg',
+//     dataCaptions: 'My summer vacation to the Oregon Coast. I love the sandy dunes!',
+//     id: pic-8;
+//   },
+// { image: '../photos/09.jpg',
+//     dataCaptions: 'We enjoyed a quiet stroll down this countryside lane',
+//     id: pic-9;
+//   },
+// { image: '../photos/10.jpg',
+//     dataCaptions: 'Sunset at the coast! The sky turned a lovely shade of orange',
+//     id: pic-10;
+//   },
+// { image: '../photos/11.jpg',
+//     dataCaptions: 'I did a tour of a cave today and the view of the landscape below was breathtaking',
+//     id: pic-11;
+//   },
+// { image: '../photos/12.jpg',
+//     dataCaptions: 'I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in',
+//     id: pic-12;
+//   },
 
-  
+// ];
+
+// //PRE VARIABLES
+// let html = '';
+// let searchInput = getElementById("search").id = "new-search";
+// let message; 
+
+
+// //LOOP TIME BRO!!!! ( i NEED TO SWITCH THE THE ORDER OF THE LOOP)
+// // I need an array to match my input to the data caption, put it in a new array, push.others > Then show it
+// //After that end with a erase
+// //I may have to create new ids in returning these values
+
+// for ( let i = 0; i < photoGallerySet.lengh, i++ ) {
+
+//   let photoGallerySet = photoGallerySet [i];
+
+
+//     if ( searchInput = `${photoGallerySet.dataCaptions}` ) {
+
+//         // Find & search for the data captions > get their ID from the propery and
+//                                               // be ready to put it back on the page
+
+//         //how to store the variable
+//         const newFilterArr = arr { };
+//           let results = `${photoGallerySet.dataCaptions}.push ` 
+
+        
+//         // #
+//         // {photoGallerySet.data-caption}` = true;
+
+//         // string them together
+
+//         html += `  ` //Need to have conver the html on teh page
+
+//     } else {
+
+//           // null and leave out time
+
+//     }
+
+//     //erease the result array
+
+// }
+
+
+// // SOME EXTRA NOTES
+// // var myElement = document.getElementById("intro");
+// // This example finds the element with id="main", and 
+// //then finds all <p> elements inside "main":
+
+
+
+// // (search = `${photoGallerySet.data-caption}` ) {
+
+// // I need an array to match my input to the data caption, put it in a new array, push.others > Then show it
+// //After that end with a erase
+// //I may have to create new ids in returning these values
+
+
+//   for let prop in photoGallerySet) {
+
+//     //I want this to filter out the information and pass what is correct
+
+//        } 
+
+
+// } else {
+
+// return: null;
+// document.getElementById("header").id 
+
+// } 
+
+
+// }
 
 
 
@@ -36,22 +159,14 @@ const search = new Filter('search', 'data-caption');
 
 
 
+//   let message = `${search} ${datacap}`;
+
+//   if (search = data-caption) {
 
 
 
-
-// function searchBar(search) {
-
-//     //Find a way to link the HMTL buttion to this input like with prompt
-// let input = document.querySelector('body').innerHTML = search;  //this should take the iput from body
-//                                                                 //look for the data and search terms
-// let message;
-
-// if (input === search) {
-// search = document.querySelector('aside').innerHTML ; //Show the search in the body
-
+//   }
 // }
 
-// }
-
+// document.querySelector('body').innerHTML = message;
 
